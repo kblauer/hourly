973dd99adca5dcd86a2616fe2db807eca21e7d84
Kyle Blauer <kyle.g.blauer@gmail.com>
2015-05-07 05:07:42 -0700

    final round 2nd commit

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from comments.models import ScheduleComment
 
 # Register your models here.
+admin.site.register(ScheduleComment)
--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from contact.models import Publisher
 
 # Register your models here.
+admin.site.register(Publisher)
